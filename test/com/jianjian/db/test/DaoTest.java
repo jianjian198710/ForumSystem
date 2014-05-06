c695273a222f3301e07376c80f5fb24e6b530ae2
jianjian <jianjian198710@hotmail.com>
2014-05-06 23:56:11 +0800

    Fix bugs in jianjian-serivce.xml and jdbc.properties
    1.fix jdbc.properties \ --> /
    2.fix jianjian-service.xml
    3.need to research the hibernateTemplate.save() FlushMode

--- test/com/jianjian/db/test/DaoTest.java
+++ test/com/jianjian/db/test/DaoTest.java
@@ -4,6 +4,8 @@ import static org.junit.Assert.*;
 
 import java.util.Date;
 
+import org.hibernate.Session;
+import org.hibernate.Transaction;
 import org.junit.After;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,21 +13,26 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.orm.hibernate4.HibernateTemplate;
 
+import com.jianjian.dao.UserDao;
 import com.jianjian.domain.User;
 
 public class DaoTest {
 
-	@Autowired
-	HibernateTemplate hibernateTemplate;
+//	@Autowired
+//	HibernateTemplate hibernateTemplate;
 	
 	//≤‚ ‘UserDao
 	@Test
 	public void test() {
 		ApplicationContext ctx = new ClassPathXmlApplicationContext("applicationContext.xml");
-		hibernateTemplate = (HibernateTemplate)ctx.getBean("hibernateTemplate");
+		UserDao userDao = (UserDao)ctx.getBean("userDao");
+		System.out.println(userDao);
+		Session session = userDao.getHibernateTemplate().getSessionFactory().openSession();
+		Transaction tr = session.beginTransaction();
 		User user = createUser();
-		hibernateTemplate.save(user);
-		
+		session.save(user);
+		tr.commit();
+		session.close();
 	}
 
 	public User createUser(){
